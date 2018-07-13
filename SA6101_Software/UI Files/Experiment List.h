6b0d40a111c1710c814118e77b3f70f8345317ac
username <niuhy@sinoagg.com>
2018-07-13 13:07:21 +0800

    22

---
+++ SA6101_Software/UI Files/Experiment List.h
@@ -0,0 +1,44 @@
+/**************************************************************************/
+/* LabWindows/CVI User Interface Resource (UIR) Include File              */
+/*                                                                        */
+/* WARNING: Do not add to, delete from, or otherwise modify the contents  */
+/*          of this include file.                                         */
+/**************************************************************************/
+
+#include <userint.h>
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+     /* Panels and Controls: */
+
+#define  EXP_LIST                         1
+#define  EXP_LIST_LISTBOX                 2       /* control type: listBox, callback function: ExpListCallBack */
+#define  EXP_LIST_LISTBOX_2               3       /* control type: listBox, callback function: (none) */
+#define  EXP_LIST_LISTBOX_3               4       /* control type: listBox, callback function: (none) */
+#define  EXP_LIST_TEXTMSG                 5       /* control type: textMsg, callback function: (none) */
+#define  EXP_LIST_TEXTMSG_3               6       /* control type: textMsg, callback function: (none) */
+#define  EXP_LIST_TEXTMSG_4               7       /* control type: textMsg, callback function: (none) */
+#define  EXP_LIST_LISTBOX_4               8       /* control type: listBox, callback function: (none) */
+#define  EXP_LIST_TEXTMSG_2               9       /* control type: textMsg, callback function: (none) */
+
+
+     /* Control Arrays: */
+
+          /* (no control arrays in the resource file) */
+
+
+     /* Menu Bars, Menus, and Menu Items: */
+
+          /* (no menu bars in the resource file) */
+
+
+     /* Callback Prototypes: */
+
+int  CVICALLBACK ExpListCallBack(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+
+
+#ifdef __cplusplus
+    }
+#endif
