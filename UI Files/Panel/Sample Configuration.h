55e6a916690c6891211cb2c126cb586fbb316126
username <niuhy@sinoagg.com>
2018-07-24 09:08:57 +0800

    V1.003

--- UI Files/Panel/Sample Configuration.h
+++ UI Files/Panel/Sample Configuration.h
@@ -24,13 +24,13 @@
 #define  SAMPLE_TEXTMSG_6                 9       /* control type: textMsg, callback function: (none) */
 
 #define  SAMPLE_CFG                       2
-#define  SAMPLE_CFG_STRING_5              2       /* control type: string, callback function: (none) */
-#define  SAMPLE_CFG_STRING_3              3       /* control type: string, callback function: (none) */
-#define  SAMPLE_CFG_I                     4       /* control type: ring, callback function: (none) */
-#define  SAMPLE_CFG_STRING_4              5       /* control type: string, callback function: (none) */
+#define  SAMPLE_CFG_STR_NUMBER            2       /* control type: string, callback function: (none) */
+#define  SAMPLE_CFG_STR_RATE              3       /* control type: string, callback function: (none) */
+#define  SAMPLE_CFG_STR_SETTING           4       /* control type: ring, callback function: (none) */
+#define  SAMPLE_CFG_STR_RUNTIME           5       /* control type: string, callback function: (none) */
 #define  SAMPLE_CFG_TEXTMSG_3             6       /* control type: textMsg, callback function: (none) */
-#define  SAMPLE_CFG_STRING_2              7       /* control type: string, callback function: (none) */
-#define  SAMPLE_CFG_STRING                8       /* control type: string, callback function: (none) */
+#define  SAMPLE_CFG_SRT_INTERVAL          7       /* control type: string, callback function: (none) */
+#define  SAMPLE_CFG_SRT_QUIET             8       /* control type: string, callback function: (none) */
 #define  SAMPLE_CFG_TEXTMSG_2             9       /* control type: textMsg, callback function: (none) */
 #define  SAMPLE_CFG_TEXTMSG_4             10      /* control type: textMsg, callback function: (none) */
 #define  SAMPLE_CFG_TEXTMSG_6             11      /* control type: textMsg, callback function: (none) */
